9e8b53dd0226e95cf99b565c72f307b4ea440815
Amr Elghobary <amr.elghobary@gmail.com>
2016-03-12 11:25:18 +0200

    add fake data for nearly a week.

--- app/src/main/java/com/amro/weatherforecast/PlaceholderFragment.java
+++ app/src/main/java/com/amro/weatherforecast/PlaceholderFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * Created by amro on 3/12/16.
  */
@@ -21,6 +23,13 @@ public class PlaceholderFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> fakeData = new ArrayList<>();
+        fakeData.add("Today - Sunny - 35/30");
+        fakeData.add("Tomorrow - Rainy - 30/20");
+        fakeData.add("Sat   - Cloudy - 20/15");
+        fakeData.add("Sun- Sunny - 35/30");
+        fakeData.add("Mon - Rainy - 30/20");
+        fakeData.add("Thru   - Cloudy - 20/15");
 
         return view;
     }
